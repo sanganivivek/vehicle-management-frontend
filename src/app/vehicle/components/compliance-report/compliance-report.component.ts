74358c1918fa39df32fcad089cf96834520c07e3
VIVEK <viveksangani588@gmail.com>
2026-01-06 16:56:11 +0530

    Initial commit

---
+++ src/app/vehicle/components/compliance-report/compliance-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compliance-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './compliance-report.component.html',
+  styleUrl: './compliance-report.component.css'
+})
+export class ComplianceReportComponent {
+
+}
--- src/app/vehicle/vehicle-routing.module.ts
+++ src/app/vehicle/vehicle-routing.module.ts
@@ -1,17 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+// Existing Components
 import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
 import { VehicleAddComponent } from './components/vehicle-add/vehicle-add.component';
 import { VehicleEditComponent } from './components/vehicle-edit/vehicle-edit.component';
 import { ModelAddComponent } from './components/model-add/model-add.component';
 import { BrandAddComponent } from './components/brand-add/brand-add.component';
 
+// You will need to create these components later for the new features
+// import { MaintenanceListComponent } from './components/maintenance-list/maintenance-list.component';
+// import { ComplianceReportComponent } from './components/compliance-report/compliance-report.component';
+
 const routes: Routes = [
+  // Fleet Routes
   { path: 'vehicle', component: VehicleListComponent },
   { path: 'vehicle/add', component: VehicleAddComponent },
   { path: 'vehicle/edit/:id', component: VehicleEditComponent },
+
+  // Master Data Routes (Now accessible from Sidebar)
+  { path: 'brands/add', component: BrandAddComponent },
   { path: 'models/add', component: ModelAddComponent },
-  { path: 'brands/add', component: BrandAddComponent }
+
+  // PLACEHOLDERS for future "Management" features
+  // { path: 'maintenance', component: MaintenanceListComponent },
+  // { path: 'reports', component: ComplianceReportComponent },
+  
+  // Default redirect
+  { path: '', redirectTo: 'vehicle', pathMatch: 'full' }
 ];
 
 @NgModule({
