78e6e5eb87710d9682f3fe627da1c1faa7a09309
VIVEK <viveksangani588@gmail.com>
2026-02-10 15:27:05 +0530

    Azure initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   // Replace 192.168.1.5 with YOUR actual local IP address
-  apiUrl: "http://192.168.1.3:5000/api",
+  apiUrl:
+    "vehicle-api-live-ahamexhyf4c3cmec.canadacentral-01.azurewebsites.net/api",
 };
